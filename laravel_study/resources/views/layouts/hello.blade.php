b36a96c4f78ec8a5684404c74b64ca59058a7922
SejunCheon <kjsug1030@naver.com>
2021-08-30 20:34:57 +0900

    First laravel

---
+++ laravel_study/resources/views/layouts/hello.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>私を操る죠르노죠바나さえ知ることはない</h1>
+@endsection
---
+++ laravel_study/resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>헤이, 모두들 안녕 내가 누군지 아니</h2>
+@endsection

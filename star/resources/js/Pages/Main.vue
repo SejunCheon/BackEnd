34139fc286e6eb5a106befc40f02e21c5b499ac5
SejunCheon <kjsug1030@naver.com>
2021-12-03 19:46:35 +0900

    add main page'

---
+++ star/resources/js/Pages/Main.vue
@@ -0,0 +1,144 @@
+<template>
+    <v-app App>
+        <v-app-bar app color="blue" elevation="4" outlined dark absolute>
+            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+            <v-toolbar-title style="">렌고쿠 쿄쥬로</v-toolbar-title>
+            <!-- <v-icon color="blue" large></v-icon> -->
+            <svg
+                aria-label="새로운 게시물"
+                class="_8-yf5"
+                color="#262626"
+                fill="#262626"
+                height="24"
+                role="img"
+                viewBox="0 0 24 24"
+                width="24"
+            >
+                <path
+                    d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552z"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                ></path>
+                <line
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    x1="6.545"
+                    x2="17.455"
+                    y1="12.001"
+                    y2="12.001"
+                ></line>
+                <line
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    x1="12.003"
+                    x2="12.003"
+                    y1="6.545"
+                    y2="17.455"
+                ></line>
+            </svg>
+        </v-app-bar>
+
+        <v-navigation-drawer v-model="drawer" absolute temporary>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="text-h6">
+                        목록
+                    </v-list-item-title>
+                    <v-list-item-subtitle> subtext </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list dense nav>
+                <v-list-item v-for="item in items" :key="item.title" link>
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <!-- Sizes your content based upon application components -->
+        <v-main>
+            <!-- Provides the application the proper gutter -->
+            <v-container fluid>
+                <!-- If using vue-router -->
+                <router-view></router-view>
+            </v-container>
+        </v-main>
+
+        <v-footer app>
+            <!-- -->
+        </v-footer>
+    </v-app>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: "렌고쿠 쿄쥬로",
+            color: "green",
+            attachRed: false,
+            drawer: false, // 사이드 바 활성화
+            items: [
+                { title: "Dashboard", icon: "mdi-view-dashboard" },
+                { title: "Photos", icon: "mdi-image" },
+                { title: "About", icon: "mdi-help-box" },
+            ],
+            right: null,
+        };
+    },
+    methods: {
+        change() {
+            this.name = "카마도 예승재";
+        },
+    },
+    computed: {
+        divClasses() {
+            return {
+                red: this.attachRed,
+                darkblue: !this.attachRed,
+            };
+        },
+    },
+};
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+}
+
+.menu {
+    padding: 15px;
+    background-color: darkblue;
+    color: white;
+}
+
+.red {
+    background-color: red;
+}
+
+.blue {
+    background-color: blue;
+}
+
+.green {
+    background-color: green;
+}
+</style>
